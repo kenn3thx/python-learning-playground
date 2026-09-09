f6decbe77c8a5b51d8acd73847ffb5f343aac1be
kenn3thx <kenn3thx@gmail.com>
2026-09-09 15:40:12 +0700

    example-03: Learn about __name__ variable and import in python

---
+++ basic/02-email-slicer/example-03.py
@@ -0,0 +1,4 @@
+from utility import func1
+
+print("Welcome to my program")
+func1();
---
+++ basic/02-email-slicer/utility.py
@@ -0,0 +1,13 @@
+def welcome():
+    print("Welcome to kenn3thx channel")
+
+def func1():
+    print("This is func1")
+
+def main():
+    welcome()
+    func1()
+
+
+if __name__ == "__main__": # If don't have this line, the main function will be called when we run example-03.py. We can print(__name__) to see __name__ variable.
+    main()
