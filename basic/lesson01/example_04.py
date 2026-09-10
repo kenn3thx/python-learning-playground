fc74f9c38b13a1d9ba1d90c7180b82f21fe67f0f
kenn3thx <kenn3thx@gmail.com>
2026-09-10 16:45:40 +0700

    fix: change example_04 of lesson01 from list to tuple

--- basic/lesson01/example_04.py
+++ basic/lesson01/example_04.py
@@ -5,20 +5,16 @@ def emailProcess(email):
     email_username = email[0:email.index("@")]
     email_domain = email[email.index("@") + 1:] # ":" to get all the characters after it 
 
-    return [email_username, email_domain]
+    return email_username, email_domain
 
 def printMsg(email_username, email_domain):
     print(f"Email username: {email_username}; Email Domain: {email_domain} ")
 
 def main():
     email = input("Please enter your email address: ").strip()
-
-    [email_username, email_domain] = emailProcess(email)
+    email_username, email_domain = emailProcess(email)
 
     printMsg(email_username, email_domain)
-
-    
-
 
 if __name__ == "__main__": # If don't have this line, when we run example_05.py, the main() function in this line will be called in example_05.
     main()
