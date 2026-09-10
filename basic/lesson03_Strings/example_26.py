846f6ba552c7b201a57a3b0813e2d2337535c08e
kenn3thx <kenn3thx@gmail.com>
2026-09-10 13:22:32 +0700

    feat: learn about Strings

--- basic/lesson03_Strings/example_26.py
+++ basic/lesson03_Strings/example_26.py
@@ -2,9 +2,9 @@ age = 24
 my_string = "I'm {} years old".format(age)
 
 
-print(my_string)
+print(my_string) # Output: I'm 24 years old
 
 height = 167.5
 my_string2 = "I'm {} years old; {:.3f}".format(age, height)
 
-print(my_string2)
+print(my_string2) # Output: I'm 24 years old; 167.500
